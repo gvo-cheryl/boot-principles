e14c939d4cbdec67c457bccb570a213d4402e311
gvo-cheryl <it.progrowth@gmail.com>
2023-03-08 17:22:26 +0900

    first commint - spring boot principles

---
+++ src/main/java/tobyspring/helloboot/HellobootSelfApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobootSelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellobootSelfApplication.class, args);
+    }
+
+}
