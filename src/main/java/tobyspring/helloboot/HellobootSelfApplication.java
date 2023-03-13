a2d4cc260d939e6475ee174a8b0d9eee0ef7c475
gvo-cheryl <it.progrowth@gmail.com>
2023-03-13 18:14:24 +0900

    독립 실행형 스프링 어플리케이션 : 스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HellobootSelfApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootSelfApplication.java
@@ -3,36 +3,35 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class HellobootSelfApplication {
 
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                // 서블릿 컨테이너 띄우기
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+                    // 서블릿 등록
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)).addMapping("/*"); // 슬래시 이후 모든 경로를 처리 한다.
+                });
+                webServer.start(); // Tomcat 서블릿 컨테이너 동작
+
+
+            }
+        };
+
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh();
+        applicationContext.refresh(); // 스프링 컨테이너의 초기화는 여기서 발생한다.
 
-        // 서블릿 컨테이너 띄우기
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-            // 서블릿 등록
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)).addMapping("/*"); // 슬래시 이후 모든 경로를 처리한다.
-        });
-        webServer.start(); // Tomcat 서블릿 컨테이너 동작
 
     }
 
