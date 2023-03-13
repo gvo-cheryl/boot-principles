3a97397742d58f2d6bb99e5e09c760c694e9ea85
gvo-cheryl <it.progrowth@gmail.com>
2023-03-13 18:28:32 +0900

    독립 실행형 스프링 어플리케이션 : 자바코드 구성 정보 사용

--- src/main/java/tobyspring/helloboot/HellobootSelfApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootSelfApplication.java
@@ -3,13 +3,28 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class HellobootSelfApplication {
 
+    @Bean
+    public HelloController helloController(HelloService helloService){
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService(){
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext () {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -28,8 +43,7 @@ public class HellobootSelfApplication {
             }
         };
 
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(HellobootSelfApplication.class);
         applicationContext.refresh(); // 스프링 컨테이너의 초기화는 여기서 발생한다.
 
 
