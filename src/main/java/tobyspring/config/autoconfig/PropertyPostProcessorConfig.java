1b64648c09ffb01a1916dad5e57eb3629ddb1059
gvo-cheryl <it.progrowth@gmail.com>
2023-03-21 10:14:17 +0900

    외부 설정을 이용한 자동 구성 : 프로퍼티 빈의 후처리기 도임 - 2

--- src/main/java/tobyspring/config/autoconfig/PropertyPostProcessorConfig.java
+++ src/main/java/tobyspring/config/autoconfig/PropertyPostProcessorConfig.java
@@ -4,20 +4,20 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.env.Environment;
 import tobyspring.config.MyAutoConfiguration;
 import tobyspring.config.MyConfigurationProperties;
 
 import java.util.Map;
 
-import static org.springframework.core.annotation.AnnotationUtils.*;
+import static org.springframework.core.annotation.AnnotationUtils.findAnnotation;
+import static org.springframework.core.annotation.AnnotationUtils.getAnnotationAttributes;
 
 @MyAutoConfiguration
 public class PropertyPostProcessorConfig {
 
     @Bean
-    BeanPostProcessor processor(Environment env){
+    BeanPostProcessor processor(Environment env) {
         return new BeanPostProcessor() {
             @Override
             public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
@@ -26,7 +26,7 @@ public class PropertyPostProcessorConfig {
                 if (annotation == null) return bean;
 
                 Map<String, Object> attr = getAnnotationAttributes(annotation);
-                String prefix = (String)  attr.get("prefix");
+                String prefix = (String) attr.get("prefix");
 
                 return Binder.get(env).bindOrCreate(prefix, bean.getClass());
             }
