8c77bf0f34bef6430e303dba77b678b3b0e353db
gvo-cheryl <it.progrowth@gmail.com>
2023-03-21 09:53:53 +0900

    외부 설정을 이용한 자동 구성 : 프로퍼티 빈의 후처리기 도임 - 1

--- src/main/java/tobyspring/config/autoconfig/ServerProperties.java
+++ src/main/java/tobyspring/config/autoconfig/ServerProperties.java
@@ -1,6 +1,9 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.stereotype.Component;
+import tobyspring.config.MyConfigurationProperties;
 
+@MyConfigurationProperties(prefix = "server")
 public class ServerProperties {
 
     private String contextPath;
--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,11 +4,13 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import tobyspring.config.MyAutoConfiguration;
 import tobyspring.config.annotation.ConditionalMyOnClass;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@Import(ServerProperties.class)
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
