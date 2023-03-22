771a3c70117c039aedadf1d0c2c5ccb98ebd38f0
gvo-cheryl <it.progrowth@gmail.com>
2023-03-22 10:54:51 +0900

    Spring JDBC 자동 구성 개발 - DataSource 자동구성 클래스

---
+++ src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,44 @@
+package tobyspring.config.autoconfig;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import tobyspring.config.EnableMyConfigurationProperties;
+import tobyspring.config.MyAutoConfiguration;
+import tobyspring.config.annotation.ConditionalMyOnClass;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)
+public class DataSourceConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+
+    @Bean
+    @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
+    DataSource hikariDataSource(MyDataSourceProperties properties){
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setDriverClassName(properties.getDriverClassName());
+        dataSource.setJdbcUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+}
