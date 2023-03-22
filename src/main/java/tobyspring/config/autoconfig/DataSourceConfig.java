ceffa5525dbfa97c87c69d5f9365180bc95b9ce4
gvo-cheryl <it.progrowth@gmail.com>
2023-03-22 11:26:37 +0900

    Spring JDBC 자동 구성 개발 - JDBCTemplate과 트랜잭션 매니저 구성

--- src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -2,8 +2,12 @@ package tobyspring.config.autoconfig;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
 import tobyspring.config.annotation.ConditionalMyOnClass;
@@ -14,6 +18,7 @@ import java.sql.Driver;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
 
     @Bean
@@ -31,7 +36,8 @@ public class DataSourceConfig {
 
     @Bean
     @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
-    DataSource hikariDataSource(MyDataSourceProperties properties){
+    @ConditionalOnMissingBean
+    DataSource hikariDataSource(MyDataSourceProperties properties) {
         HikariDataSource dataSource = new HikariDataSource();
 
         dataSource.setDriverClassName(properties.getDriverClassName());
@@ -41,4 +47,19 @@ public class DataSourceConfig {
 
         return dataSource;
     }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
+    }
+
 }
--- src/test/java/tobyspring/helloboot/DataSourceTest.java
+++ src/test/java/tobyspring/helloboot/DataSourceTest.java
@@ -1,19 +1,13 @@
 package tobyspring.helloboot;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = HellobootSelfApplication.class)
-@TestPropertySource("classpath:/application.properties")
+@HelloBootTest
 public class DataSourceTest {
 
     @Autowired
@@ -22,7 +16,7 @@ public class DataSourceTest {
     @Test
     void connect() throws SQLException {
         Connection connection = dataSource.getConnection();
-        connection.close();;
+        connection.close();
     }
 
 }
